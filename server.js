4080360b05d0a53781655c6795cf927eb7b9a0fe
JohnQuimson <john.quimson777@gmail.com>
2024-05-21 15:28:03 +0100

    feat: function per stampare random frase

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 require('dotenv').config();
 const http = require('http');
 const port = process.env.PORT;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST;
+const message = process.env.MESSAGE;
 
 const frasiMotivazionali = [
-  ' La vita è per il 10% cosa ti accade e per il 90% come reagisci.',
-  'Ci sono due regole nella vita: 1. Non mollare mai; 2. Non dimenticare mai la regola n° 1. ',
+  'La vita e` per il 10% cosa ti accade e per il 90% come reagisci.',
+  'Ci sono due regole nella vita: \n1. Non mollare mai; \n2. Non dimenticare mai la regola n° 1. ',
   'Se qualcosa non ti piace, cambiala. Se non puoi cambiarla, cambia il tuo atteggiamento. Non lamentarti.',
   'Se vuoi qualcosa che non hai mai avuto, devi fare qualcosa che non hai mai fatto.',
-  'Non essere una "se-potessi-sarei o una se-volessi-potrei-essere". Sii e basta. ',
+  'Non essere una "se potessi sarei o una se volessi potrei essere". Sii e basta.',
   'Il mio scopo nella vita? Fare solo cose positive.',
   'Prima ti ignorano, poi ti deridono, poi ti combattono. Poi vinci.',
-  'Non è mai troppo tardi per essere ciò che avresti voluto essere',
-  'È nel momento delle decisioni che si plasma il tuo destino.',
-  'Niente è davvero difficile se lo si divide in tanti piccoli pezzettini.',
+  'Non e` mai troppo tardi per essere cio` che avresti voluto essere',
+  'E` nel momento delle decisioni che si plasma il tuo destino.',
+  'Niente e` davvero difficile se lo si divide in tanti piccoli pezzettini.',
 ];
+
+const GetFrase = () => {
+  const index = Math.floor(Math.random() * frasiMotivazionali.length);
+  return `${index}. ${frasiMotivazionali[index]}`;
+};
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, {
       'Content-Type': 'text/html',
     });
-    res.end('Hello World!');
+    const randomFrase = GetFrase();
+    res.end(`.env message: ${message}\n
+      <h1>${randomFrase}<h1/>
+    `);
   })
   .listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
