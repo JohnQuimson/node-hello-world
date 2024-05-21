589553f76e957dcccff3c4f9970c871fa8d7c60e
JohnQuimson <john.quimson777@gmail.com>
2024-05-21 16:55:44 +0100

    feat: added charset=utf-8

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const host = process.env.HOST || 'missing HOST';
 const message = process.env.MESSAGE || 'missing MESSAGE';
 
 const frasiMotivazionali = [
-  'La vita e` per il 10% cosa ti accade e per il 90% come reagisci.',
+  'La vita è per il 10% cosa ti accade e per il 90% come reagisci.',
   'Ci sono due regole nella vita: \n1. Non mollare mai; \n2. Non dimenticare mai la regola n° 1. ',
   'Se qualcosa non ti piace, cambiala. Se non puoi cambiarla, cambia il tuo atteggiamento. Non lamentarti.',
   'Se vuoi qualcosa che non hai mai avuto, devi fare qualcosa che non hai mai fatto.',
   'Non essere una "se potessi sarei o una se volessi potrei essere". Sii e basta.',
   'Il mio scopo nella vita? Fare solo cose positive.',
   'Prima ti ignorano, poi ti deridono, poi ti combattono. Poi vinci.',
-  'Non e` mai troppo tardi per essere cio` che avresti voluto essere',
-  'E` nel momento delle decisioni che si plasma il tuo destino.',
-  'Niente e` davvero difficile se lo si divide in tanti piccoli pezzettini.',
+  'Non è mai troppo tardi per essere ciò che avresti voluto essere',
+  'È nel momento delle decisioni che si plasma il tuo destino.',
+  'Niente è davvero difficile se lo si divide in tanti piccoli pezzettini.',
 ];
 
 const GetFrase = () => {
@@ -26,14 +26,14 @@ http
   .createServer(function (req, res) {
     if (req.url === '/favicon.ico') {
       res.writeHead(404, {
-        'Content-Type': 'text/html',
+        'Content-Type': 'text/html;charset=utf-8',
       });
       res.end('<h1>404 Not Found</h1>');
       return;
     }
 
     res.writeHead(200, {
-      'Content-Type': 'text/html',
+      'Content-Type': 'text/html;charset=utf-8',
     });
 
     const randomFrase = GetFrase();
